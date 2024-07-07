809bfced98894fdd9fc68ab07fc260ef6aae2324
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-07-07 20:30:53 +0200

    fix: extends interface Authenticators

--- apps/shared/middleware/auth_middleware.ts
+++ apps/shared/middleware/auth_middleware.ts
@@ -1,6 +1,5 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
-import type { Authenticators } from '@adonisjs/auth/types'
 
 /**
  * Auth middleware is used authenticate HTTP requests and deny
@@ -22,4 +21,9 @@ export default class AuthMiddleware {
     await ctx.auth.authenticateUsing(options.guards, { loginRoute: this.redirectTo })
     return next()
   }
+}
+
+interface Authenticators {
+  jwt: 'jwt'
+  api: 'api'
 }
