7d2388787a70f58807ddab120652d8111a20d8d0
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-07-19 09:29:53 +0200

    feat: Add activity.ts validator for creating and updating activities

---
+++ apps/activity/validators/activity.ts
@@ -0,0 +1,21 @@
+import vine from '@vinejs/vine'
+import { Infer } from '@vinejs/vine/types'
+
+/**
+ * Validator to validate the payload when creating
+ * a new activity.ts.
+ */
+export const createActivityValidator = vine.compile(
+  vine.object({})
+)
+
+/**
+ * Validator to validate the payload when updating
+ * an existing activity.ts.
+ */
+export const updateActivityValidator = vine.compile(
+  vine.object({})
+)
+
+export type CreateActivitySchema = Infer<typeof createActivityValidator>
+export type UpdateActivitySchema = Infer<typeof updateActivityValidator>
