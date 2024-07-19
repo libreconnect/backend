4a48e3fb7594abd8d8f7c17dfe31ee3f9b988d45
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-07-19 09:30:46 +0200

    feat: Update PatientsController to include pagination for patient listing

--- apps/patient/controllers/patients_controller.ts
+++ apps/patient/controllers/patients_controller.ts
@@ -1,22 +1,46 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { inject } from '@adonisjs/core'
 import PatientService from '#apps/patient/services/patient_service'
-import { createPatientValidator } from '#apps/patient/validators/patient'
+import { createPatientValidator, patientsQueryValidator } from '#apps/patient/validators/patient'
 
 @inject()
 export default class PatientsController {
   constructor(private patientService: PatientService) {}
 
+  /**
+   * @index
+   * @summary List all patients
+   * @responseBody 200 - <Patient[]>.paginated()
+   * @responseBody 403 - Forbidden
+   * @responseBody 401 - Unauthorized
+   * @paramQuery page - Page number - @type(number)
+   * @paramQuery limit - Number of items per page - @type(number)
+   */
+  async index({ request }: HttpContext) {
+    const { page, limit } = await request.validateUsing(patientsQueryValidator)
+
+    return this.patientService.findAll({ page, limit })
+  }
+
+  /**
+   * @show
+   * @summary Show a patient
+   * @reponseBody 200 - <Patient>
+   * @responseBody 403 - Forbidden
+   * @responseBody 401 - Unauthorized
+   */
   async show({ params }: HttpContext) {
     return this.patientService.findById(params.id)
   }
 
   /**
    * @store
+   * @summary Create a patient
+   * @description Returns array of producs and it's relations
    * @responseBody 201 - <Patient>
    * @responseBody 403 - Forbidden
    * @responseBody 401 - Unauthorized
-   * @requestBody <CreatePatientSchema>
+   * @requestBody <createPatientValidator>
    */
   async store({ request }: HttpContext) {
     const data = await request.validateUsing(createPatientValidator)
