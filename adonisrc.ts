2484084fa50e02a05cdacf246dbf181047919007
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-07-19 09:29:20 +0200

    chore: Update adonisrc.ts and package.json for @adonisjs/bouncer integration

--- adonisrc.ts
+++ adonisrc.ts
@@ -10,7 +10,11 @@ export default defineConfig({
   | will be scanned automatically from the "./commands" directory.
   |
   */
-  commands: [() => import('@adonisjs/core/commands'), () => import('@adonisjs/lucid/commands')],
+  commands: [
+    () => import('@adonisjs/core/commands'),
+    () => import('@adonisjs/lucid/commands'),
+    () => import('@adonisjs/bouncer/commands'),
+  ],
 
   /*
   |--------------------------------------------------------------------------
@@ -32,6 +36,7 @@ export default defineConfig({
     () => import('@adonisjs/cors/cors_provider'),
     () => import('@adonisjs/lucid/database_provider'),
     () => import('@adonisjs/auth/auth_provider'),
+    () => import('@adonisjs/bouncer/bouncer_provider'),
   ],
 
   /*
