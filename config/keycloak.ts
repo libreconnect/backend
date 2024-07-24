d886ea30fbdeb7246a57434e268de1d1fbd3557b
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-07-24 02:49:47 +0200

    refactor: Update keycloak configuration to use environment variables

--- config/keycloak.ts
+++ config/keycloak.ts
@@ -1,9 +1,12 @@
 import { KeycloakConfig } from '#apps/authentication/services/keycloak_service'
+import env from '#start/env'
 
 const keycloakConfig: KeycloakConfig = {
-  realm: 'libreconnect',
-  url: 'http://localhost:8080',
+  realm: env.get('KEYCLOAK_REALM'),
+  url: env.get('KEYCLOAK_URL'),
   admin: {},
+  clientId: env.get('KEYCLOAK_CLIENT_ID'),
+  clientSecret: env.get('KEYCLOAK_CLIENT_SECRET'),
 }
 
 export default keycloakConfig
