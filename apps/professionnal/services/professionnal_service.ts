e8adb8f07eaf1afcd49281483152aad9438ba4cc
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-07-24 02:25:25 +0200

    refactor: Remove unused code and update exception handling in ProfessionalException

--- apps/professionnal/services/professionnal_service.ts
+++ apps/professionnal/services/professionnal_service.ts
@@ -1,11 +1,21 @@
 import ProfessionalException from '#apps/professionnal/exceptions/professionnal_exception'
 import Professionnal from '#apps/shared/models/professionnal'
 import { CreateProfessionnalSchema } from '#apps/professionnal/validators/professionnal'
+import logger from '@adonisjs/core/services/logger'
 
 export default class ProfessionnalService {
   async findByOidcId(oidcId: string) {
     try {
-      return Professionnal.query().where('oidc_id', oidcId).firstOrFail()
+      logger.info('Fetching professionnal with oidc id %s', oidcId)
+      const professionnal = await Professionnal.query().where('oidc_id', oidcId).first()
+
+      if (!professionnal) {
+        throw new ProfessionalException('Professional not found', {
+          code: 'E_PROFESSIONNAL_NOT_FOUND',
+          status: 404,
+        })
+      }
+      return professionnal
     } catch (err) {
       throw new ProfessionalException('Professional not found', {
         code: 'E_PROFESSIONNAL_NOT_FOUND',
