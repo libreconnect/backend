62e047175b830956b86305dc572a6a4764ec97f3
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-07-25 22:54:46 +0200

    test: list.spec for patients

--- tests/functional/patients/list.spec.ts
+++ tests/functional/patients/list.spec.ts
@@ -1,24 +1,18 @@
-import KeycloakService from '#apps/authentication/services/keycloak_service'
 import Patient from '#apps/shared/models/patient'
-import { keycloakUsers } from '#config/app'
-import app from '@adonisjs/core/services/app'
 import { test } from '@japa/runner'
+import Professionnal from '#models/professionnal'
 
 test.group('Patients list', () => {
   test('should return a 403 error when a patient user accesses /v1/patients', async ({
     assert,
     client,
   }) => {
-    const keycloakService = await app.container.make(KeycloakService)
+    const patient = await Patient.firstOrFail()
 
-    const accessToken = await keycloakService.createAccessToken(
-      keycloakUsers.PATIENT.username,
-      keycloakUsers.PATIENT.password
-    )
 
     const response = await client
       .get('/v1/patients')
-      .header('Authorization', `Bearer ${accessToken}`)
+      .loginAs(patient)
 
     response.assertStatus(403)
 
@@ -30,17 +24,11 @@ test.group('Patients list', () => {
     client,
     assert,
   }) => {
-    const keycloakService = await app.container.make(KeycloakService)
-
-    const accessToken = await keycloakService.createAccessToken(
-      keycloakUsers.PROFESSIONNEL.username,
-      keycloakUsers.PROFESSIONNEL.password
-    )
+    const professional = await Professionnal.firstOrFail()
 
     const response = await client
       .get('/v1/patients')
-      .header('Authorization', `Bearer ${accessToken}`)
-
+      .loginAs(professional)
     response.assertStatus(200)
     assert.properties(response.body(), ['data', 'meta'])
   }).tags(['patients'])
@@ -53,13 +41,4 @@ test.group('Patients list', () => {
     assert.properties(response.body(), ['code', 'message', 'status'])
     assert.equal(response.body().code, 'E_AUTHENTICATION_UNAUTHORIZED')
   }).tags(['patients'])
-
-  test('tgest', async ({ client }) => {
-    const patient = await Patient.firstOrFail()
-
-    const response = await client.get('/v1/patients').loginAs(patient)
-    console.log(response.body())
-
-    //.header('Authorization', `Bearer ${accessToken}`)
-  }).tags(['test'])
 })
