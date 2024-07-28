774804cd4bfccf587c621bc23b40b3ca8e571e1d
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-07-29 00:49:40 +0200

    chore: add log in company service

--- apps/companies/services/company_service.ts
+++ apps/companies/services/company_service.ts
@@ -1,6 +1,7 @@
 import { Exception } from '@adonisjs/core/exceptions'
 import Company from '#apps/shared/models/company'
 import { CreateCompaniesSchema, GetCompaniesSchema } from '#apps/companies/validators/company'
+import logger from '@adonisjs/core/services/logger'
 
 export default class CompanyService {
   async findAll({ page = 1, limit = 10, nationalCode }: GetCompaniesSchema) {
@@ -12,7 +13,10 @@ export default class CompanyService {
   }
   async create(payload: CreateCompaniesSchema): Promise<Company> {
     try {
-      return Company.create(payload)
+      const company = await Company.create(payload)
+      logger.info(company.$attributes, 'Company created')
+
+      return company
     } catch (err) {
       throw new Exception('Failed to create company', {
         code: 'E_COMPANY_CREATE',
