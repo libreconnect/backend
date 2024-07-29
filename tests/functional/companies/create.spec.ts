bb945c5089167e27f1599d5b06257ed7069d183c
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-07-29 16:45:04 +0200

    test: add new test for create company

--- tests/functional/companies/create.spec.ts
+++ tests/functional/companies/create.spec.ts
@@ -3,7 +3,10 @@ import Professional from '#models/professional'
 import { Roles } from '#apps/shared/interfaces/roles'
 
 test.group('Companies create', () => {
-  test('example test', async ({ assert, client }) => {
+  test('should create a company and return a 201 status if the authenticated user has the necessary permission', async ({
+    assert,
+    client,
+  }) => {
     const professional = await Professional.firstOrFail()
 
     const response = await client
@@ -29,6 +32,36 @@ test.group('Companies create', () => {
       'phone',
       'nationalCode',
     ])
-    response.assertStatus(200)
+    response.assertStatus(201)
   }).tags(['companies'])
 })
+
+test('should return a 403 error if the authenticated user does not have the necessary permission', async ({
+  assert,
+  client,
+}) => {
+  const professional = await Professional.firstOrFail()
+
+  const response = await client.post('/v1/companies').loginAs(professional, []).json({
+    name: 'Company name',
+    address: 'Company address',
+    city: 'Company city',
+    country: 'Company country',
+    zipCode: 'Company zipCode',
+    phone: 'Company phone',
+    nationalCode: 'DZAD82D71D12',
+  })
+
+  response.assertStatus(403)
+  assert.properties(response.body(), ['code', 'message', 'status'])
+  assert.equal(response.body().code, 'E_COMPANY_UNAUTHORIZED')
+}).tags(['companies'])
+
+test('should return a 401 error if the user is not logged in', async ({ client, assert }) => {
+  const response = await client.post('/v1/companies')
+
+  response.assertStatus(401)
+
+  assert.properties(response.body(), ['code', 'message', 'status'])
+  assert.equal(response.body().code, 'E_AUTHENTICATION_UNAUTHORIZED')
+}).tags(['companies'])
