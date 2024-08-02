9345e211b61a0afa9e954e201eab69a4058a6e08
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-08-02 16:55:43 +0200

    chore: update policy and add role
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- apps/companies/policies/company_policy.ts
+++ apps/companies/policies/company_policy.ts
@@ -4,10 +4,14 @@ import { inject } from '@adonisjs/core'
 import RoleService from '#apps/shared/services/role_service'
 import CompanyException from '#apps/companies/exceptions/company_exception'
 import { Roles } from '#apps/shared/interfaces/roles'
+import ProfessionalService from '#apps/professional/services/professional_service'
 
 @inject()
 export default class CompanyPolicy extends BasePolicy {
-  constructor(private roleService: RoleService) {
+  constructor(
+    private roleService: RoleService,
+    private professionalService: ProfessionalService
+  ) {
     super()
   }
 
@@ -17,6 +21,27 @@ export default class CompanyPolicy extends BasePolicy {
         status: response.status,
         code: 'E_COMPANY_UNAUTHORIZED',
       })
+    }
+  }
+
+  async view(payload: JWTPayload) {
+    if (this.roleService.verifyAccess(payload, Roles.COMPANY_VIEWER)) {
+      return AuthorizationResponse.allow()
+    }
+
+    return AuthorizationResponse.deny('You are not authorized to view companies', 403)
+  }
+
+  async show(payload: JWTPayload, companyId: string) {
+    try {
+      const professional = await this.professionalService.findByOidcId(payload.sub)
+      if (!professional || professional.companyId !== companyId) {
+        return AuthorizationResponse.deny('You are not authorized to view this company', 403)
+      }
+
+      return AuthorizationResponse.allow()
+    } catch (error) {
+      return AuthorizationResponse.deny('You are not authorized to view this company', 403)
     }
   }
 
--- apps/shared/interfaces/roles.ts
+++ apps/shared/interfaces/roles.ts
@@ -1,3 +1,4 @@
 export enum Roles {
   COMPANY_CREATOR = 'company_creator',
+  COMPANY_VIEWER = 'view_company',
 }
