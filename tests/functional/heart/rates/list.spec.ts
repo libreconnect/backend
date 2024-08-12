c33e5163c77fdd56059d0b5ec83671e72dc0fc0f
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-08-12 15:05:33 +0200

    test: write test for heart rates
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- tests/functional/heart/rates/list.spec.ts
+++ tests/functional/heart/rates/list.spec.ts
@@ -1,5 +1,6 @@
 import { test } from '@japa/runner'
 import Professional from '#models/professional'
+import Patient from '#models/patient'
 
 test.group('Heart Rates - List', () => {
   test('should handle non-existing patient when retrieving heart rates', async ({
@@ -22,5 +23,24 @@ test.group('Heart Rates - List', () => {
 
     assert.properties(response.body(), ['code', 'message', 'status'])
     assert.equal(response.body().code, 'E_AUTHENTICATION_UNAUTHORIZED')
+  }).tags(['heart'])
+
+  test('should get heart data for the similar patient', async ({ assert, client }) => {
+    const patient = await Patient.findByOrFail('oidc_id', 'f875c155-9607-4aa3-af4e-59b3bff848ff')
+
+    const response = await client.get(`/v1/heart/rates/${patient.id}`).loginAs(patient, [])
+    response.assertStatus(200)
+    assert.properties(response.body(), ['meta', 'data'])
+  }).tags(['heart'])
+
+  test('should return a 403 error if the user is not the same user', async ({ assert, client }) => {
+    const patient = await Patient.findByOrFail('oidc_id', 'f875c155-9607-4aa3-af4e-59b3bff848ff')
+
+    const response = await client
+      .get('/v1/heart/rates/9498994932837253124945211')
+      .loginAs(patient, [])
+    response.assertStatus(403)
+    assert.properties(response.body(), ['message', 'status', 'code'])
+    assert.equal(response.body().code, 'E_PATIENT_HEART_UNAUTHORIZED')
   }).tags(['heart'])
 })
