42bbe16bf0e8eeebfa9336603beef615775e1bd4
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-08-14 06:52:05 +0200

    feat: update policy and use this in controller
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- apps/heart/policies/patient_heart_rates_policy.ts
+++ apps/heart/policies/patient_heart_rates_policy.ts
@@ -1,11 +1,29 @@
 import { AuthorizationResponse, BasePolicy } from '@adonisjs/bouncer'
 import { JWTPayload } from '#apps/authentication/contracts/jwt'
 import Patient from '#models/patient'
+import HeartException from '#apps/heart/exceptions/heart_exception'
 
 export default class PatientHeartRatesPolicy extends BasePolicy {
-  async create(_payload: JWTPayload, patient: Patient, patientId: string) {
-    if (patient.id === patientId) return true
+  async after(_payload: JWTPayload, _action: string, response: AuthorizationResponse) {
+    if (!response.authorized) {
+      throw new HeartException(response.message, {
+        status: response.status,
+        code: 'E_PATIENT_HEART_UNAUTHORIZED',
+      })
+    }
+  }
 
-    return AuthorizationResponse.deny('E_HEART_CREATION_UNAUTHORIZED', 403)
+  async view(payload: JWTPayload, patient: Patient) {
+    if (patient.oidcId === payload.sub) {
+      return AuthorizationResponse.allow()
+    }
+
+    return AuthorizationResponse.deny("You can't show the heart data to this patient", 403)
+  }
+
+  async create(_payload: JWTPayload, patient: Patient, patientId: string) {
+    if (patient.id === patientId) return AuthorizationResponse.allow()
+
+    return AuthorizationResponse.deny("You can't create a heart data for this user", 403)
   }
 }
